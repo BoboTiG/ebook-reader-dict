1b9358178704ac1552fa3bae054ded149c1d4f4d
Mickaël Schoentgen <contact@tiger-222.fr>
2020-04-17 15:08:08 +0200

    Complete tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,56 @@
+import bz2
 import json
 import os
 from pathlib import Path
+from xml.sax.saxutils import escape
 
 import pytest
 
 os.environ["CI"] = "1"
 os.environ["CWD"] = str(Path(__file__).parent)
+
+
+@pytest.fixture(scope="session")
+def craft_data():
+    def _data(date, locale):
+        data_dir = Path(os.environ["CWD"]) / "data" / locale
+        pages_xml = data_dir / f"pages-{date}.xml"
+        pages_bz2 = data_dir / f"pages-{date}.xml.bz2"
+
+        with pages_xml.open(mode="w", encoding="utf-8") as fh:
+            fh.write(f'<mediawiki xml:lang="{locale}">\n')
+            xml = """\
+<page>
+    <title>{word}</title>
+    <ns>0</ns>
+    <id>1</id>
+    <revision>
+        <id>222</id>
+        <parentid>221</parentid>
+        <timestamp>2020-04-17T14:21:12Z</timestamp>
+        <contributor>
+            <username>Alice</username>
+            <id>42</id>
+        </contributor>
+        <comment>/**/</comment>
+        <model>wikitext</model>
+        <format>text/x-wiki</format>
+        <text xml:space="preserve">{text}</text>
+    </revision>
+</page>
+"""
+            for file in data_dir.glob("*.wiki"):
+                text = file.read_text(encoding="utf-8")
+                text = escape(text)
+                fh.write(xml.format(word=file.stem, text=text))
+            fh.write("</mediawiki>\n")
+
+        # Compress the XML
+        with pages_xml.open(mode="rb") as fi, bz2.open(pages_bz2, "wb") as fo:
+            fo.writelines(fi)
+
+    return _data
+
 
 
 @pytest.fixture
--- tests/test_fr_0_get.py
+++ tests/test_fr_0_get.py
@@ -1,9 +1,13 @@
 import os
 from pathlib import Path
+from unittest.mock import patch
 
 import pytest
 
 os.environ["WIKI_LOCALE"] = "fr"
+
+# Must be imported after *WIKI_LOCALE* is set
+import get  # noqa
 
 
 @pytest.mark.parametrize(
@@ -13,7 +17,6 @@ os.environ["WIKI_LOCALE"] = "fr"
         ("aux", "o"),
         ("barbe à papa", "baʁ.b‿a pa.pa"),
         ("Bogotanais", "bɔ.ɡɔ.ta.nɛ"),
-        ("en", "ɑ̃"),
         ("pinyin", "pin.jin"),
         ("suis", "sɥi"),
         ("Slovène", "slɔ.vɛn"),
@@ -21,10 +24,8 @@ os.environ["WIKI_LOCALE"] = "fr"
 )
 def test_find_pronunciation(word, pron, page):
     """Test the pronunciation finder."""
-    from get import find_pronunciation
-
     data = page(word)
-    text = find_pronunciation(data["revision"]["text"]["#text"])
+    text = get.find_pronunciation(data["revision"]["text"]["#text"])
     assert text == pron
 
 
@@ -40,10 +41,8 @@ def test_find_pronunciation(word, pron, page):
 )
 def test_find_genre(word, genre, page):
     """Test the genre finder."""
-    from get import find_genre
-
     data = page(word)
-    text = find_genre(data["revision"]["text"]["#text"])
+    text = get.find_genre(data["revision"]["text"]["#text"])
     assert text == genre
 
 
@@ -76,19 +75,6 @@ def test_find_genre(word, genre, page):
                 "Pourvoir d’une occupation ou d’un travail pour son usage ou pour son profit.",
             ],
         ),
-        (
-            "en",
-            [
-                "Code ISO 639-1 (alpha-2) de l’anglais.",
-                "Utilisé après certains verbes.",
-                "Permet de préciser une matière.",
-                "Indique le lieu.",
-                "De ça.",
-                "Personne, chose de cette espèce, d’entre eux, d’entre elles. Utilisé tout seul ou avec une quantification.",
-                "De ça. Pour une personne, on utilise de lui, d’elle, etc. ou un adjectif possessif.",
-                "De ce lieu. De là. De ce côté-là",
-            ],
-        ),
         ("ich", ["Code de l’etkywan."]),
         (
             "pinyin",
@@ -110,14 +96,12 @@ def test_find_genre(word, genre, page):
 )
 def test_find_sections_and_definitions(word, defs, page):
     """Test the sections finder definitions getter."""
-    from get import find_definitions, find_sections
-
     data = page(word)
-    sections = find_sections(data["revision"]["text"]["#text"])
+    sections = get.find_sections(data["revision"]["text"]["#text"])
 
     res = []
     for section in sections:
-        res.extend(find_definitions(section))
+        res.extend(get.find_definitions(section))
     assert res == defs
 
 
@@ -136,20 +120,49 @@ def test_find_sections_and_definitions(word, defs, page):
 )
 def test_is_ignored(word, ignored):
     """Test words filtering."""
-    from get import is_ignored
-
-    assert is_ignored(word) is ignored
+    assert get.is_ignored(word) is ignored
 
 
-def test_main(data):
+@patch("get.fetch_snapshots")
+def test_main(mocked_fetch_snapshots, craft_data):
     """Test the whole script. It will generate data for test_fr_1_convert.py."""
-    from get import main
+
+    # Fake
+    date = "20200417"
+
+    data_dir = Path(os.environ["CWD"]) / "data" / "fr"
+    data = data_dir / "data.json"
+    count = data_dir / "words.count"
+    snapshot = data_dir / "words.snapshot"
+    wordlist = data_dir / "words.list"
+    pages_xml = data_dir / f"pages-{date}.xml"
+    pages_bz2 = data_dir / f"pages-{date}.xml.bz2"
+
+    # Clean-up before we start
+    for file in (data, count, snapshot, wordlist, pages_xml, pages_bz2):
+        file.unlink()
+
+    # fetch_snapshots() should return the specific date to prevent downloading anything
+    mocked_fetch_snapshots.return_value = [date]
+    assert get.fetch_snapshots() == [date]
+
+    # Generate the *pages* file using data from the test folder
+    craft_data(date, "fr")
+    assert pages_xml.is_file()
+    assert pages_bz2.is_file()
 
     # Start the whole process
-    main()
+    assert get.main() == 0
 
     # Check for generated files
-    data_dir = Path(os.environ["CWD"]) / "data" / "fr"
-    assert (data_dir / "words.count").is_file()
-    assert (data_dir / "words.list").is_file()
-    assert (data_dir / "words.snapshot").is_file()
+    # Here we do -1 because "Bogotanais.wiki" will not work (no definition found)
+    expected_count = len(list(data_dir.glob("*.wiki"))) - 1
+    assert int(count.read_text()) == expected_count
+    assert snapshot.read_text() == "20200417"
+    words = wordlist.read_text(encoding="utf-8").splitlines()
+    assert len(words) == expected_count
+    for line in words:
+        assert len(line.split("|")) == 2
+
+    # Call it again and no new action should be made
+    assert get.main() == 1
