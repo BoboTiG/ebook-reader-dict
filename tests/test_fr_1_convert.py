50a0b922f9d0525956c33d56607578a438dabcde
Mickaël Schoentgen <contact@tiger-222.fr>
2020-04-17 15:08:08 +0200

    Complete tests

--- tests/test_fr_1_convert.py
+++ tests/test_fr_1_convert.py
@@ -3,22 +3,24 @@ from pathlib import Path
 
 os.environ["WIKI_LOCALE"] = "fr"
 
+# Must be imported after *WIKI_LOCALE* is set
+import convert  # noqa
+
 
 def test_main(data):
     """Test the JSON -> HTML conversion."""
-    from convert import main
-
     # Start the whole process
-    main()
+    assert convert.main() == 0
 
     # Check for individual HTML files
-    data_dir = Path(os.environ["CWD"]) / "data" / "fr""
-    for group in ("br", "ce"):
-        assert (data_dir / "tmp" / f"{group}.raw.html").is_file()
-        assert (data_dir / "tmp" / f"{group}.html").is_file()
+    data_dir = Path(os.environ["CWD"]) / "data" / "fr"
+    data_tmp = data_dir / "tmp"
+    for group in ("ac", "au", "ba", "em", "ic", "l’", "œc", "pi", "qu", "sl", "su"):
+        assert (data_tmp / f"{group}.raw.html").is_file()
+        assert (data_tmp / f"{group}.html").is_file()
 
     # Check for the special "words" file
-    assert (data_dir / "tmp" / "words").is_file()
+    assert (data_tmp / "words").is_file()
 
     # Check for the final ZIP file
     assert (data_dir / f"dicthtml-fr.zip").is_file()
