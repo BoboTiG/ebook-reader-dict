305f67719cce0a30cc8ddbdbfb2bc7d7ad626e17
Mickaël Schoentgen <contact@tiger-222.fr>
2020-04-17 22:54:15 +0200

    Test the ZIP content as well

--- tests/test_fr_1_convert.py
+++ tests/test_fr_1_convert.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+from zipfile import ZipFile
 
 os.environ["WIKI_LOCALE"] = "fr"
 
 # Must be imported after *WIKI_LOCALE* is set
+from scripts import constants as C  # noqa
 from scripts import convert  # noqa
 
 
@@ -12,18 +14,26 @@ def test_main(data):
     # Start the whole process
     assert convert.main() == 0
 
-    # Check for individual HTML files
-    data_dir = Path(os.environ["CWD"]) / "data" / "fr"
-    data_tmp = data_dir / "tmp"
-    for group in ("ac", "au", "ba", "em", "ic", "l’", "œc", "pi", "qu", "sl"):
-        assert (data_tmp / f"{group}.raw.html").is_file()
-        assert (data_tmp / f"{group}.html").is_file()
-
-    # Check for the special "words" file
-    assert (data_tmp / "words").is_file()
-
     # Check for the final ZIP file
-    assert (data_dir / f"dicthtml-fr.zip").is_file()
+    dicthtml = C.SNAPSHOT / f"dicthtml-fr.zip"
+    assert dicthtml.is_file()
 
-    # Check ZIP content
-    # TODO
+    # Check the ZIP content
+    with ZipFile(dicthtml) as fh:
+        expected = [
+            "qu.html",
+            "sl.html",
+            "ac.html",
+            "au.html",
+            "ba.html",
+            "em.html",
+            "ic.html",
+            "l’.html",
+            "mo.html",
+            "pi.html",
+            "œc.html",
+            "words",
+            "words.count",
+            "words.snapshot",
+        ]
+        assert fh.namelist() == expected
