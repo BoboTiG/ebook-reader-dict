f843a1be02bd0419c2565a2e798866bf7f88cf99
Mickaël Schoentgen <contact@tiger-222.fr>
2020-04-17 17:25:51 +0200

    Move convert.py and get.py into scripts/
    Also refactored get.py to prevent global vars.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,26 +6,51 @@ from xml.sax.saxutils import escape
 
 import pytest
 
-os.environ["CI"] = "1"
 os.environ["CWD"] = str(Path(__file__).parent)
 
 
-@pytest.fixture(scope="session")
-def craft_data():
-    def _data(date, locale):
-        data_dir = Path(os.environ["CWD"]) / "data" / locale
-        pages_xml = data_dir / f"pages-{date}.xml"
-        pages_bz2 = data_dir / f"pages-{date}.xml.bz2"
+XML = """<mediawiki xml:lang="{locale}">
+<siteinfo>
+    <sitename>Wiktionnaire</sitename>
+    <dbname>frwiktionary</dbname>
+    <base>https://fr.wiktionary.org/wiki/Wiktionnaire:Page_d%E2%80%99accueil</base>
+    <generator>MediaWiki 1.35.0-wmf.25</generator>
+    <case>case-sensitive</case>
+    <namespaces>
+        <namespace key="-2" case="case-sensitive">Média</namespace>
+        <namespace key="-1" case="first-letter">Spécial</namespace>
+        <namespace key="0" case="case-sensitive" />
+    </namespaces>
+</siteinfo>
 
-        with pages_xml.open(mode="w", encoding="utf-8") as fh:
-            fh.write(f'<mediawiki xml:lang="{locale}">\n')
-            xml = """\
+<!-- To cover word containing a ":" -->
 <page>
+    <title>MediaWiki:Sitetitle</title>
+    <ns>8</ns>
+    <id>12</id>
+    <revision>
+        <id>403956</id>
+        <parentid>33016</parentid>
+        <timestamp>2006-02-13T09:08:31Z</timestamp>
+        <contributor>
+        <username>Bob</username>
+        <id>-42</id>
+        </contributor>
+        <comment>changement de titre pour meilleur référencement dans les moteurs de recherche</comment>
+        <model>wikitext</model>
+        <format>text/x-wiki</format>
+        <text bytes="46" xml:space="preserve">Wiktionnaire : dictionnaire libre et universel</text>
+        <sha1>40helna9646ffk0utvwm8bkdlzi1eck</sha1>
+    </revision>
+</page>
+
+"""
+PAGE_XML = """<page>
     <title>{word}</title>
     <ns>0</ns>
     <id>1</id>
     <revision>
-        <id>222</id>
+        <id>{revision}</id>
         <parentid>221</parentid>
         <timestamp>2020-04-17T14:21:12Z</timestamp>
         <contributor>
@@ -39,17 +64,46 @@ def craft_data():
     </revision>
 </page>
 """
-            for file in data_dir.glob("*.wiki"):
-                text = file.read_text(encoding="utf-8")
-                text = escape(text)
-                fh.write(xml.format(word=file.stem, text=text))
-            fh.write("</mediawiki>\n")
 
-        # Compress the XML
-        with pages_xml.open(mode="rb") as fi, bz2.open(pages_bz2, "wb") as fo:
-            fo.writelines(fi)
 
-    return _data
+@pytest.fixture(scope="session")
+def craft_data():
+    def _craft_data(date, locale, to_add=None, to_remove=None, to_alter=None):
+        """
+        """
+
+        data_dir = Path(os.environ["CWD"]) / "data" / locale
+        content = XML.format(locale=locale)
+        for file in data_dir.glob("*.wiki"):
+            revision = 42
+
+            # Possitility to remove a word
+            if to_remove and file.stem in to_remove:
+                print(f"[fixture] Removed word {file.stem!r}")
+                continue
+
+            text = file.read_text(encoding="utf-8")
+
+            # Possibility to alter the content of a word
+            if to_alter and file.stem in to_alter:
+                print(f"[fixture] Altered word {file.stem!r}")
+                revision += 1
+
+            text = escape(text)
+            content += PAGE_XML.format(word=file.stem, revision=revision, text=text)
+
+        # Possibility to add new words
+        if to_add:
+            skeleton = (data_dir / "page.skeleton").read_text(encoding="utf-8")
+            for addition in to_add:
+                print(f"[fixture] Added word {addition['word']!r}")
+                addition["text"] = escape(skeleton.replace("WORD", addition["word"]))
+                content += PAGE_XML.format(**addition)
+
+        content += "</mediawiki>"
+        return bz2.compress(content.encode("utf-8"))
+
+    return _craft_data
 
 
 @pytest.fixture
