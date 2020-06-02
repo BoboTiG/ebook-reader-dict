c5c44bb1544338922c719344ec010cf000e1a19a
Mickaël Schoentgen <contact@tiger-222.fr>
2020-06-02 15:47:16 +0200

    New locale: Catalan

--- scripts/lang/__init__.py
+++ scripts/lang/__init__.py
@@ -1,7 +1,7 @@
 """Internationalization stuff."""
 import re
 
-from . import fr
+from . import ca, fr
 from .fr.langs import langs as FR
 
 # A list of all languages translated into different locales
@@ -11,45 +11,54 @@ all_langs = {
 
 # Some definitions are not good to keep (plural, genre, ... )
 definitions_to_ignore = {
+    "ca": ca.definitions_to_ignore,
     "fr": fr.definitions_to_ignore,
 }
 
 # But some words need to be kept even if they would have been skipped by definitions_to_ignore
 words_to_keep = {
+    "ca": ca.words_to_keep,
     "fr": fr.words_to_keep,
 }
 
 # Regex to find the pronunciation
 pronunciation = {
+    "ca": re.compile(ca.pronunciation),
     "fr": re.compile(fr.pronunciation),
 }
 
 # Regex to find the genre
 genre = {
+    "ca": re.compile(ca.genre),
     "fr": re.compile(fr.genre),
 }
 
 # Markers for sections that contain interesting text to analyse.
 head_sections = {
+    "ca": ca.head_sections,
     "fr": fr.head_sections,
 }
 sections = {
+    "ca": ca.sections,
     "fr": fr.sections,
 }
 
 # The template name dealing with files in the Wikicode.
 # [[Fichier:...|...|...|...]] (fr)
 # [[File:...|...|...|...]] (fr)
+# [[Fitxer:...|...|...]] (ca)
 # [[Image:...|...|...]] (fr)
-pattern_file = ("Fichier", "File", "Image")
+pattern_file = ("Fichier", "File", "Fitxer", "Image")
 
 # Float number separator
 float_separator = {
+    "ca": ca.float_separator,
     "fr": fr.float_separator,
 }
 
 # Thousads separator
 thousands_separator = {
+    "ca": ca.thousands_separator,
     "fr": fr.thousands_separator,
 }
 
@@ -57,6 +66,7 @@ thousands_separator = {
 
 # Templates to ignore: the text will be deleted.
 templates_ignored = {
+    "ca": ca.templates_ignored,
     "fr": fr.templates_ignored,
 }
 
@@ -66,6 +76,7 @@ templates_ignored = {
 # Ex: {{absol|fr|123}} -> <i>(Absolument)</i>
 # Ex: {{absol|fr|123|...}} -> <i>(Absolument)</i>
 templates_italic = {
+    "ca": ca.templates_italic,
     "fr": fr.templates_italic,
 }
 
@@ -86,27 +97,32 @@ templates_italic = {
 # Available functions are documented into that HTML file:
 #   html/scripts/user_functions.html
 templates_multi = {
+    "ca": ca.templates_multi,
     "fr": fr.templates_multi,
 }
 
 # Templates that will be completed/replaced using custom style.
 templates_other = {
+    "ca": ca.templates_other,
     "fr": fr.templates_other,
 }
 
 # A warning will be printed when a template contains superfuous spaces,
 # except for those listed bellow:
 templates_warning_skip = {
+    "ca": ca.templates_warning_skip,
     "fr": fr.templates_warning_skip,
 }
 
 # The full release description on GitHub:
 # https://github.com/BoboTiG/ebook-reader-dict/releases/tag/$LOCALE
 release_description = {
+    "ca": ca.release_description,
     "fr": fr.release_description,
 }
 
 # Dictionary name that will be printed below each definition
 wiktionary = {
+    "ca": ca.wiktionary,
     "fr": fr.wiktionary,
 }
---
+++ scripts/lang/ca/__init__.py
@@ -0,0 +1,106 @@
+"""Catalan language."""
+
+pronunciation = r"{{ca-pron\|(?:or=)?/([^/\|]+)"
+genre = r"{{ca-\w+\|([fm]+)"
+
+float_separator = ","
+thousands_separator = "."
+
+head_sections = ("{-ca-}", "{-mul-}")
+sections = (
+    "Abreviatura",
+    "Acrònim",
+    "Adjectiu",
+    "Adverbi",
+    "Article",
+    "Contracció",
+    "Infix",
+    "Interjecció",
+    "Lletra",
+    "Nom",
+    "Numeral",
+    "Prefix",
+    "Preposició",
+    "Pronom",
+    "Sigles",
+    "Sufix",
+    "Símbol",
+    "Verb",
+)
+
+definitions_to_ignore = (
+    # Ignore conjuged verbs
+    "ca-forma-conj",
+    # Proper nouns
+    "cognom",
+    "prenom",
+    # Ignore genres
+    "forma-f",
+    # Ignore plurals
+    "forma-p",
+)
+words_to_keep = tuple()  # type: ignore
+
+templates_ignored = tuple()  # type: ignore
+
+templates_other = {
+    "m": "m.",
+}
+
+templates_italic = {
+    "arcaic": "arcaisme",
+    "fruits": "botànica",
+    "plantes": "botànica",
+}
+
+templates_multi = {
+    # {{color|#E01010}}
+    "color": "color(parts[1])",
+    # {{e|la|lupus}}
+    "e": "parts[2]",
+    # {{forma-a|ca|Beget}}
+    "forma-a": "f\"{italic('forma alternativa de')} {strong(parts[2])}\"",
+    # {{forma-pron|ca|estimar}}
+    "forma-pron": "f\"{italic('forma pronominal de')} {strong(parts[2])}\"",
+    # {{IPAchar|[θ]}}
+    "IPAchar": "parts[-1]",
+    # {{marca|ca|fruits}}
+    # {{marca|ca|interrogatiu|condicional}}
+    "marca": "term(lookup_italic(concat(parts, sep=', ', indexes=[2, 3, 4, 5]), 'ca'))",
+    # {{marca-nocat|ca|balear}}
+    # {{marca-nocat|ca|occidental|balear}}
+    "marca-nocat": "term(lookup_italic(concat(parts, sep=', ', indexes=[2, 3, 4, 5]), 'ca'))",
+    # {{q|tenir bona planta}}
+    "q": "term(parts[-1])",
+    # {{terme|it|come}}
+    "terme": "parts[-1]",
+}
+
+templates_warning_skip = tuple()  # type: ignore
+
+release_description = """\
+Words count: {words_count}
+Wiktionary dump: {dump_date}
+
+:arrow_right: Download: [dicthtml-ca.zip]({url})
+
+---
+
+Installation:
+
+1. Copy the `dicthtml-ca.zip` file into the `.kobo/dict/` folder of the reader.
+2. Restart the reader.
+
+---
+
+Caractéristics :
+
+- Only definitions are included: there is no quote nor ethymology.
+- 1-character words are not included.
+- Proper nouns are not included.
+- Conjuged verbs are not included.
+
+<sub>Updated on {creation_date}</sub>
+"""
+
+wiktionary = "Viccionari (ɔ) {year}"
