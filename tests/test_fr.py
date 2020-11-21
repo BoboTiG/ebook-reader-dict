50152763f81713c220639fbb31863f6eb68412f1
lasconic <lasconic@gmail.com>
2020-11-21 10:44:52 +0100

    fix #236: [FR] Handle graphie Template

--- tests/test_fr.py
+++ tests/test_fr.py
@@ -385,6 +385,7 @@ def test_parse_word(
         ("{{info lex|équitation|sport}}", "<i>(Équitation, Sport)</i>"),
         ("[[J·K-1|'''J·K{{e|-1}}''']]", "<b>J·K<sup>-1</sup></b>"),
         ("{{FR|fr}}", "<i>(France)</i>"),
+        ("{{graphie|u}}", "‹&nbsp;u&nbsp;›"),
         ("{{lang|en|other rank}}", "other rank"),
         ("{{Lang|la|[[Martis]] [[dies]]}}", "Martis dies"),
         ("{{langues|fr|de Chine}}", "<i>(Linguistique)</i>"),
