dfedccf3b9e12a69110a25189ee6493afac8813d
Mickaël Schoentgen <contact@tiger-222.fr>
2021-01-08 17:41:53 +0100

    fix #520: [CA] Ignore the 'catllengua' template
    Ex: https://ca.wiktionary.org/wiki/ofim%C3%A0tica

--- wikidict/lang/ca/__init__.py
+++ wikidict/lang/ca/__init__.py
@@ -56,6 +56,7 @@ definitions_to_ignore = (
 # Templates to ignore: the text will be deleted.
 templates_ignored = (
     "categoritza",
+    "catllengua",
     "manquen accepcions",
     "sense accepcions",
     "-etimologia-",
