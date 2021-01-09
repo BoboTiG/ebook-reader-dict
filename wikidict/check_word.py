9c640a3c790de611aa64c29968c2587e32a81761
Mickaël Schoentgen <contact@tiger-222.fr>
2021-01-09 15:55:19 +0100

    --check-word: Small refactoring

--- wikidict/check_word.py
+++ wikidict/check_word.py
@@ -1,43 +1,51 @@
-"""Get and render a word."""
+"""Get and render a word; then compare with the rendering done on the Wiktionary to catch errors."""
+from .render import parse_word
+from .stubs import Word
+
 import requests
 from bs4 import BeautifulSoup
 
-from .render import parse_word
-from .stubs import Word
+
+def get_text(html: str) -> str:
+    """Parse the HTML code and return it as a string."""
+    return str(BeautifulSoup(markup=html, features="html.parser").text)
 
 
 def get_word(word: str, locale: str) -> Word:
     """Get a *word* wikicode and parse it."""
     url = f"https://{locale}.wiktionary.org/w/index.php?title={word}&action=raw"
     with requests.get(url) as req:
-        code = req.text
-
-    return parse_word(word, code, locale, force=True)
+        return parse_word(word, req.text, locale)
 
 
 def get_wiktionary_page(word: str, locale: str) -> str:
+    """Get a *word* HTML."""
     url = f"https://{locale}.wiktionary.org/w/index.php?title={word}"
-    req = requests.get(url)
-    page = req.content
-    bs = BeautifulSoup(page, features="html.parser")
-    return str(bs.get_text())
+    with requests.get(url) as req:
+        return get_text(req.text)
 
 
 def main(locale: str, word: str) -> int:
     """Entry point."""
     details = get_word(word, locale)
     text = get_wiktionary_page(word, locale)
-    if details.etymology:
-        clean_text = BeautifulSoup(details.etymology, features="html.parser").text
-        if clean_text not in text:
-            print("Etymology KO")
-            print(clean_text)
+    errors = 0
 
-    for definition in details.definitions:
-        if definition:
-            clean_text = BeautifulSoup(definition, features="html.parser").text
-            if clean_text not in text:
-                print("definition KO")
-                print(clean_text)
+    if details.etymology:
+        clean_text = get_text(details.etymology)
+        if clean_text not in text:
+            print(" !! Etymology")
+            print(clean_text)
+            errors += 1
+
+    for num, definition in enumerate(details.definitions, start=1):
+        clean_text = get_text(definition)
+        if clean_text not in text:
+            print(f"\n !! Definition n°{num}")
+            print(clean_text)
+            errors += 1
+
+    if errors:
+        print("\n >>> Errors:", errors)
 
     return 0
