651315cbf62b1eebd104d14b4d054bcaec80fead
BoboTiG <BoboTiG@users.noreply.github.com>
2021-03-06 18:35:28 +0000

    Update locale-specific data

--- wikidict/lang/es/langs.py
+++ wikidict/lang/es/langs.py
@@ -569,6 +569,7 @@ langs = {
     "kyr": "kuruaya",
     "kyz": "kayabí",
     "la": "latín",
+    "la-med": "latín medieval",
     "la-vul": "latín vulgar",
     "lac": "lacandón",
     "lad": "judeoespañol",
@@ -1175,5 +1176,5 @@ langs = {
     "zrp": "judeofrancés",
     "zu": "zulú",
     "zun": "zuñi",
-}  # 1,170
+}  # 1,171
 # END
