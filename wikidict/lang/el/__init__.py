d82d7bb2d2702bc0021730c8a42ce2048b2b2536
chopinesque <afaton@yahoo.com>
2021-07-05 18:55:41 +0300

    Update __init__.py

--- wikidict/lang/el/__init__.py
+++ wikidict/lang/el/__init__.py
@@ -9,16 +9,17 @@ pronunciation = r"{ΔΦΑ(?:\|γλ=el)?\|([^}\|]+)"
 # '''{{PAGENAME}}''' {{θ}}
 # '''{{PAGENAME}}''' {{ο}}
 # '''{{PAGENAME}}''' {{α}}
+# '''{{PAGENAME}}''' {{αθ}}
 gender = r"'''{{PAGENAME}}''' \{\{([θαο])\}\}"
 
 # Float number separator
 float_separator = ","
 
 # Thousands separator
-thousands_separator = " "
+thousands_separator = "."
 
 # Markers for sections that contain interesting text to analyse.
-head_sections = ("{{-el-}}",)
+head_sections = ("{{-el-}}")
 etyl_section = ["{{ετυμολογία}}"]
 sections = (
     *head_sections,
@@ -30,32 +31,47 @@ sections = (
     "{{επίθετο}",
     "{{επίθετο|el}",
     "{{επίρρημα}",
+    "{{επίρρημα|el}",
     "{{σύνδεσμος}",
+    "{{σύνδεσμος|el}",
     "{{συντομομορφή}",
+    "{{συντομομορφή|el}",
     "{{κύριο όνομα}",
+    "{{κύριο όνομα|el}",
     "{{αριθμητικό}",
+    "{{αριθμητικό|el}",
     "{{άρθρο}",
+    "{{άρθρο|el}",
     "{{μετοχή}",
+    "{{μετοχή|el}",
     "{{μόριο}",
+    "{{μόριο|el}",
     "{{αντωνυμία}",
+    "{{αντωνυμία|el}",
     "{{επιφώνημα}",
+    "{{επιφώνημα|el}",
     "{{ρηματική έκφραση}",
     "{{επιρρηματική έκφραση}",
 )
 
 # Some definitions are not good to keep (plural, gender, ... )
 definitions_to_ignore = (
-    "{{μορφή ουσιαστικού",
-    "{{μορφή ρήματος",
-    "{{μορφή επιθέτου",
-    "{{εκφράσεις",
+    "{{μορφή ουσιαστικού}",
+    "{{μορφή ουσιαστικού|el}",
+    "{{μορφή ρήματος}",
+    "{{μορφή ρήματος|el}",
+    "{{μορφή επιθέτου}",
+    "{{μορφή επιθέτου|el}",
+    "{{εκφράσεις}",
+    "{{εκφράσεις|el}",
 )
 
 # Templates to ignore: the text will be deleted.
 templates_ignored = (
-    "el-κλίσ",
+    "el-κλίση",
     "!",
     "R:TELETERM",
+    "κλείδα-ελλ",
 )
 
 # Templates that will be completed/replaced using italic style.
@@ -79,7 +95,7 @@ release_description = """\
 - [StarDict]({url_stardict}) (dict-{locale}.zip)
 - [DictFile]({url_dictfile}) (dict-{locale}.df)
 
-<sub>Ημερομηνία δημιουργίας: {creation_date}</sub>
+<sub>Ημερομηνία δημιουργίας {creation_date}</sub>
 """  # noqa
 
 # Dictionary name that will be printed below each definition
