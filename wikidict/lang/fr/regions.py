df675ce5c4be9996f50a93e2d7818a4d35475969
BoboTiG <BoboTiG@users.noreply.github.com>
2021-08-23 00:32:31 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -387,6 +387,7 @@ regions = {
     "burkina": "Burkina Faso",
     "lyonnais": "Lyonnais",
     "léonard": "Léonard",
+    "normand insulaire": "Normand insulaire",
     "parler gaga": "Parler gaga",
     "pays de Waes": "Pays de Waes",
     "régio": "Régionalisme",
@@ -403,7 +404,7 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 398
+}  # 399
 # END
 
 # handled differently
