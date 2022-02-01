e84db3e49e3b2c28fb36ed1a0363f818e288b277
Mickaël Schoentgen <contact@tiger-222.fr>
2022-02-01 14:40:57 +0100

    Fix --help output

--- wikidict/__main__.py
+++ wikidict/__main__.py
@@ -22,7 +22,7 @@ Options:
   --convert                 Convert rendered data to working dictionaries into several files:
                                 - "data/$LOCALE/dicthtml-$LOCALE-$LOCALE.zip": Kobo format.
                                 - "data/$LOCALE/dict-$LOCALE-$LOCALE.df.bz2": DictFile format.
-                                - "data/$LOCALE/dict-$LOCALE-$LOCALE.zip": StarFile format.
+                                - "data/$LOCALE/dict-$LOCALE-$LOCALE.zip": StarDict format.
   --find-templates          DEBUG: Find all templates in use.
   --check-words             Render words, then compare with the rendering done on the Wiktionary to catch errors.
                             --random            randomly if --random
@@ -31,7 +31,7 @@ Options:
                             --input=FILENAME    A list of words, one by line
                             --check-word=WORD   Get and render WORD.
   --get-word=WORD [--raw]   Get and render WORD. Pass --raw to ouput the raw HTML code.
-  --gen-dict=WORDS          DEBUG: Generate Kobo/StarSict dictionaries for specific words. Pass multiple words
+  --gen-dict=WORDS          DEBUG: Generate Kobo/StarDict dictionaries for specific words. Pass multiple words
                             separated with a comma: WORD1,WORD2,WORD3,...
                             The generated filename can be tweaked via the --output=FILENAME argument.
                             --format= FORMAT    Format can be "kobo" or "stardict" [default: kobo]
