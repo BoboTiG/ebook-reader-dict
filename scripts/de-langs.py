863a7edd6526e5419db43892dfbaf619f2241f45
Nicolas Froment <lasconic@gmail.com>
2022-02-19 21:49:18 +1300

    more fixes for DE locale. See #1161 (#1231)

--- scripts/de-langs.py
+++ scripts/de-langs.py
@@ -2,35 +2,56 @@ import requests
 from bs4 import BeautifulSoup
 
 
+ROOT_URL = "https://de.wiktionary.org"
+START_URL = f"{ROOT_URL}/wiki/Kategorie:Wiktionary:Sprachk%C3%BCrzel"
+NEXTPAGE_TEXT = "nächste Seite"
+
+ALIAS_URL = "https://de.wiktionary.org/w/index.php?title=Spezial:Linkliste/{}&hidetrans=1&hidelinks=1"
+
+
 def get_soup(url):
     req = requests.get(url)
     page = req.content
     return BeautifulSoup(page, features="html.parser")
 
 
-root_url = "https://de.wiktionary.org"
-start_url = f"{root_url}/wiki/Kategorie:Wiktionary:Sprachadjektive"
-alias_url = "https://de.wiktionary.org/w/index.php?title=Spezial:Linkliste/{}&hidetrans=1&hidelinks=1"
-soup = get_soup(start_url)
+def process_page(page_url, languages):
+    soup = get_soup(page_url)
 
-content = soup.find("div", {"class": "mw-category"})
-lis = content.findAll("li")
+    nextpage = ""
+    nextpage_div = soup.find(id="mw-pages")
+    last_link = nextpage_div.find_all("a")[-1]
+    if NEXTPAGE_TEXT == last_link.text:
+        nextpage = ROOT_URL + last_link.get("href")
+
+    content = soup.find("div", {"class": "mw-category"})
+    lis = content.findAll("li")
+    for li in lis:
+        link = li.find("a")["href"]
+        li_url = ROOT_URL + link
+        key = li.text.split(":")[1]
+        sub_soup = get_soup(li_url)
+        content = sub_soup.find("div", {"class": "mw-parser-output"}).find(
+            "p", recursive=False
+        )
+        value = content.text.strip()
+        languages[key] = value
+        a_url = ALIAS_URL.format(li.text)
+        soup_alias = get_soup(a_url)
+        if ul_alias := soup_alias.find("ul", {"id": "mw-whatlinkshere-list"}):
+            for alias_li in ul_alias.findAll("li"):
+                alias_text = alias_li.find("a").text
+                alias_key = alias_text.split(":")[1]
+                languages[alias_key] = value
+
+    return nextpage
+
+
+next_page_url = START_URL
 languages = {}
-for li in lis:
-    link = li.find("a")["href"]
-    li_url = root_url + link
-    key = li.text.split(":")[1]
-    sub_soup = get_soup(li_url)
-    content = sub_soup.find("div", {"class": "mw-parser-output"}).find("p")
-    value = content.text.strip()
-    languages[key] = value
-    a_url = alias_url.format(li.text)
-    soup_alias = get_soup(a_url)
-    if ul_alias := soup_alias.find("ul", {"id": "mw-whatlinkshere-list"}):
-        for alias_li in ul_alias.findAll("li"):
-            alias_text = alias_li.find("a").text
-            alias_key = alias_text.split(":")[1]
-            languages[alias_key] = value
+
+while next_page_url:
+    next_page_url = process_page(next_page_url, languages)
 
 
 print("langs = {")
