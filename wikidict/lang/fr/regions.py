1ec85fcba08875fbf25c5839b610370d78eacf46
BoboTiG <BoboTiG@users.noreply.github.com>
2022-02-21 00:09:54 +0000

    Update locale-specific data

--- wikidict/lang/fr/regions.py
+++ wikidict/lang/fr/regions.py
@@ -349,12 +349,9 @@ regions = {
     "San Cataldo": "San Cataldo",
     "San Fratello": "San Fratello",
     "Santo Stefano Quisquina": "Santo Stefano Quisquina",
-    "Sarthe": "Sarthe",
     "Saumur": "Saumur",
     "Savoie": "Savoie",
     "Segré": "Segré",
-    "Sercq": "Sercq",
-    "Seychelles": "Seychelles",
     "Singapour": "Singapour",
     "Slovaquie": "Slovaquie",
     "Sologne": "Sologne",
@@ -431,7 +428,7 @@ regions = {
     "Île-de-France": "Île-de-France",
     "Îles d’Aran": "Îles d'Aran",
     "Îles-de-la-Madeleine": "Îles-de-la-Madeleine",
-}  # 426
+}  # 423
 # END
 
 # handled differently
