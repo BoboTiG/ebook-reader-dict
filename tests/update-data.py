c8c611f317bfceb7bc9fc7ed7c6b9f06bcafcc7a
Mickaël Schoentgen <contact@tiger-222.fr>
2022-11-14 21:20:57 +0100

    tests: auto-update HTML data files

--- tests/update-data.py
+++ tests/update-data.py
@@ -4,20 +4,29 @@ from pathlib import Path
 import requests
 
 
+def fetch_and_store_if_updated(file: Path, url: str) -> None:
+    current_content = file.read_text().strip()
+    with requests.get(url) as req:
+        if not req.ok:
+            return
+        new_content = req.text.strip()
+    if current_content != new_content:
+        file.write_text(req.text + "\n")
+        print(f"Updated {file}", flush=True)
+
+
 def main():
     url_fmt = "https://{}.wiktionary.org/w/index.php?title={}&action=raw"
     folder = Path(__file__).parent / "data"
     for locale in folder.iterdir():
         for file in locale.glob("*.wiki"):
-            current_content = file.read_text().strip()
             url = url_fmt.format(locale.name, file.stem)
-            with requests.get(url) as req:
-                if not req.ok:
-                    continue
-                new_content = req.text.strip()
-            if current_content != new_content:
-                file.write_text(req.text + "\n")
-                print(f"Updated {file}", flush=True)
+            fetch_and_store_if_updated(file, url)
+
+            html_file = file.with_suffix(".html")
+            if html_file.is_file():
+                url = url.replace("&action=raw", "")
+                fetch_and_store_if_updated(html_file, url)
 
 
 if __name__ == "__main__":
