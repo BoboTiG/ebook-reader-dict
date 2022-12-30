44ffd2232fd32aa8a6c83586dc4760643307840b
Mickaël Schoentgen <contact@tiger-222.fr>
2022-12-30 06:49:44 +0100

    CI: try to workaround network issues, and to reduce workflow steps (#1459)

---
+++ wikidict/release.py
@@ -0,0 +1,13 @@
+"""Generate the description of a GitHub release."""
+import os
+from pathlib import Path
+
+from .utils import format_description
+
+
+def main(locale: str) -> int:
+    """Entry point."""
+
+    output_dir = Path(os.getenv("CWD", "")) / "data" / locale
+    print(format_description(locale, output_dir), flush=True)
+    return 0
