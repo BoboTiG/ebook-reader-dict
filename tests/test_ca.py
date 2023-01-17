8c6962e7eef5369bb14bcd80af33eecf2b0157ad
Mickaël Schoentgen <contact@tiger-222.fr>
2023-01-17 21:28:41 +0100

    [CA] Support "fals tall" templates

--- tests/test_ca.py
+++ tests/test_ca.py
@@ -244,6 +244,11 @@ def test_parse_word(
         ("{{comp|ca|-oma}}", "sufix <i>-oma</i>"),
         ("{{doblet|ca|Castellar}}", "<i>Castellar</i>"),
         ("{{e|la|longifolius|longifolia}}", "longifolia"),
+        (
+            "{{fals tall|ca|[[sarna]] > [[s']]arna}}",
+            "fals tall sil·làbic de <i>sarna > s'arna</i>",
+        ),
+        ("{{Fals tall|ca|Far|el Far}}", "Fals tall sil·làbic de <i>el Far</i>"),
         ("{{IPAchar|[θ]}}", "[θ]"),
         ("{{pron|ca|/kənˈta/}}", "/kənˈta/"),
         ("{{pron|en|/əˈkrɔs/|/əˈkrɑs/}}", "/əˈkrɔs/, /əˈkrɑs/"),
