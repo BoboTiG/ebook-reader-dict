18294d4ba93b7ed2029c0a7bc56b434dbf9df710
Mickaël Schoentgen <contact@tiger-222.fr>
2023-01-19 09:52:01 +0100

    fix #1579: [PT] Wrong gramatica for "trans" (#1588)

--- scripts/pt-gramatica.py
+++ scripts/pt-gramatica.py
@@ -1,33 +1,30 @@
-import re
-from typing import List
+from typing import Dict, List
 
-from scripts_utils import get_soup
+from scripts_utils import get_content
 
-url = "https://pt.wiktionary.org/w/index.php?title=Predefini%C3%A7%C3%A3o:gram%C3%A1tica/core&action=edit"
-soup = get_soup(url)
-textarea = soup.find("textarea")
+url = "https://pt.wiktionary.org/w/index.php?title=Predefini%C3%A7%C3%A3o:gram%C3%A1tica/core&action=raw"
+text = get_content(url)
 
-current_abbr: List[str] = []
-count = 0
-
-text = textarea.text
 text = text.replace("{{#ifeq:{{int:Log}}|{{:MediaWiki:Log}}|género|gênero}}", "género")
 text = text.replace("{{gramática/core/faltagenero|{{{2|}}}}}", "gênero em falta")
-text = re.sub("(<!--.*?-->)", "", text, flags=re.DOTALL)
+text = "".join(line for line in text.splitlines() if line[0] == "|" and line[1] != " ")
+text = text.lstrip("|")
+
+gramaticas: Dict[str, str] = {}
+current_abbr: List[str] = []
+
+for p in text.split("|"):
+    if "=" in p:
+        key, value = p.split("=")
+        value = value.strip("'")
+        gramaticas[key] = value
+        for abbr in current_abbr:
+            gramaticas[abbr] = value
+        current_abbr.clear()
+    else:
+        current_abbr.append(p)
 
 print("gramatica_short = {")
-for p in sorted(text.split("|")):
-    p = p.strip()
-    if p and "<!--" not in p and ("{" not in p) and ("}" not in p):
-        if "=" in p:
-            sArray = p.split("=")
-            name = sArray[1].strip("'")
-            print(f'    "{sArray[0]}": "{name}",')
-            count += 1
-            for abbr in sorted(current_abbr):
-                print(f'    "{abbr}": "{name}",')
-                count += 1
-            current_abbr.clear()
-        else:
-            current_abbr.append(p)
-print(f"}}  # {count:,}")
+for key, value in sorted(gramaticas.items()):
+    print(f'    "{key}": "{value}",')
+print(f"}}  # {len(gramaticas):,}")
