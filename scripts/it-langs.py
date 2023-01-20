605eadfae8aa705593ee7f5438a3eccce982cbe6
Mickaël Schoentgen <contact@tiger-222.fr>
2023-01-20 22:08:28 +0100

    [IT] Support langs single template, second try

--- scripts/it-langs.py
+++ scripts/it-langs.py
@@ -1,11 +1,42 @@
-from scripts_utils import get_content
+from typing import Dict
 
-url = "https://it.wiktionary.org/w/api.php?action=query&meta=languageinfo&liprop=name&format=json"
-content = get_content(url, as_json=True)
-langs = content["query"]["languageinfo"]
+from scripts_utils import get_soup
+
+ROOT = "https://it.wiktionary.org"
+START_URL = f"{ROOT}/wiki/Categoria:Template_lingua_testo"
+NEXTPAGE_TEXT = "pagina successiva"
+
+
+def process_page(url: str, results: Dict[str, str]) -> str:
+    soup = get_soup(url)
+
+    nextpage = ""
+    nextpage_div = soup.find(id="mw-pages")
+    last_link = nextpage_div.find_all("a")[-1]
+    if NEXTPAGE_TEXT == last_link.text:
+        nextpage = ROOT + last_link.get("href")
+
+    content_div = soup.find("div", "mw-category-generated")
+    lis = content_div.find_all("li")
+    for li in lis:
+        try:
+            tpl_name = li.text.split(":")[1]
+        except IndexError:
+            continue
+        tpl_url = ROOT + li.find("a").get("href")
+        tpl_soup = get_soup(tpl_url)
+        lang = tpl_soup.find("div", "mw-parser-output").find("a").text
+        results[tpl_name] = lang
+    return nextpage
+
+
+results: Dict[str, str] = {}
+
+next_page_url = START_URL
+while next_page_url:
+    next_page_url = process_page(next_page_url, results)
 
 print("langs = {")
-for iso, lang in sorted(langs.items()):
-    name = lang["name"]
-    print(f'    "{iso}": "{name}",')
-print(f"}}  # {len(langs):,}")
+for key, value in sorted(results.items()):
+    print(f'    "{key}": "{value}",')
+print(f"}}  # {len(results):,}")
