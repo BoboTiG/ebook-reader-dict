9917e4b3bc08ec7c52c81e93d8f5b9545dfec1d0
Mickaël Schoentgen <contact@tiger-222.fr>
2023-01-22 16:45:03 +0100

    Fetch namespace aliases (#1653)
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- scripts/all-namespaces.py
+++ scripts/all-namespaces.py
@@ -2,20 +2,26 @@ from typing import Dict, List
 
 from scripts_utils import get_content
 
-url = "https://{0}.wiktionary.org/w/api.php?action=query&meta=siteinfo&siprop=namespaces&format=json"
+url = "https://{0}.wiktionary.org/w/api.php?action=query&meta=siteinfo&siprop={1}&format=json"
+
+# https://en.wiktionary.org/wiki/Wiktionary:Namespace
+ids = {6, 14}  # File, and Category
 
 results: Dict[str, List[str]] = {}
 locales = ("ca", "de", "el", "en", "es", "fr", "it", "no", "pt", "ru", "sv")
 
 for locale in locales:
     result_discard_last: List[str] = []
-    json = get_content(url.format(locale), as_json=True)
-    namespaces = json["query"]["namespaces"]
-    for key, namespace in namespaces.items():
-        if n := namespace["*"]:
-            if namespace["canonical"] in ["File", "Category"]:
-                result_discard_last.append(n)
-    results[locale] = result_discard_last
+    for kind in ("namespaces", "namespacealiases"):
+        json = get_content(url.format(locale, kind), as_json=True)
+        data = json["query"][kind]
+        if kind == "namespaces":
+            result_discard_last.extend(data[str(id_)]["*"] for id_ in ids)
+        else:
+            result_discard_last.extend(
+                namespace["*"] for namespace in data if namespace["id"] in ids
+            )
+    results[locale] = sorted(result_discard_last)
 
 print("namespaces =", end=" ")
 print(results)
