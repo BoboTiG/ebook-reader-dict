39561f2f708ec0d38c2f6930723c9f4d08ebf21f
Mickaël Schoentgen <contact@tiger-222.fr>
2023-01-23 13:10:06 +0100

    fix #1676: [IT] Clean-up "<gallery>" code

--- wikidict/utils.py
+++ wikidict/utils.py
@@ -294,7 +294,7 @@ def clean(text: str, locale: str = "en") -> str:
         '{{nom langue|gcr}}'
         >>> clean("[[Annexe:Principales puissances de 10|10{{e|&minus;6}}]] [[gray#fr-nom|gray]]", locale="fr")
         '10{{e|&minus;6}} gray'
-        >>> clean("[[Fichier:Blason ville fr Petit-Bersac 24.svg|vignette|120px|'''Base''' d’or ''(sens héraldique)'']]", locale="fr")  # noqa
+        >>> clean("[[Fichier:Blason ville fr Petit-Bersac 24.svg|vignette|120px|'''Base''' d’or ''(sens héraldique)'']]", locale="fr")
         ''
         >>> clean("[[File:Sarcoscypha_coccinea,_Salles-la-Source_(Matthieu_Gauvain).JPG|vignette|Pézize écarlate]]", locale="en")
         ''
@@ -341,7 +341,10 @@ def clean(text: str, locale: str = "en") -> str:
         ''
         >>> clean("A_____B, B_____A")
         'A_____B, B_____A'
-    """
+
+        >>> clean("<gallery>\nImage: Hydra (creature).jpg|due idre minacciose\nImage: Hydre.jpg|idra minacciosa\nImage: Chateauneuf-Randon de Joyeuse.svg|d'oro, a tre pali d'azzurro; al capo di rosso caricato di tre idre minacciose del campo<br /></gallery>")
+        ''
+    """  # noqa
 
     # Speed-up lookup
     sub = re.sub
@@ -375,6 +378,9 @@ def clean(text: str, locale: str = "en") -> str:
 
     # <nowiki/> -> ''
     text = text.replace("<nowiki/>", "")
+
+    # <gallery
+    text = sub(r"<gallery>[\s\S]*?</gallery>", "", text)
 
     # Local links
     text = sub(r"\[\[([^||:\]]+)\]\]", "\\1", text)  # [[a]] -> a
