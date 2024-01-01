ea2b9c67dc84d98f1e4017da441171b1abc58c6d
Mickaël Schoentgen <contact@tiger-222.fr>
2024-01-01 12:00:58 +0100

    doc: Update dates

--- wikidict/__init__.py
+++ wikidict/__init__.py
@@ -1,16 +1,14 @@
 """
 Finally descent dictionaries based on Wiktionary for your beloved eBook reader.
 
-This module is maintained by Mickaël Schoentgen.
-
 You can always get the latest version of this module at:
     https://github.com/BoboTiG/ebook-reader-dict
 """
 
-__version__ = "3.0.0"
+__version__ = "4.0.0"
 __author__ = "Mickaël 'Tiger-222' Schoentgen"
 __copyright__ = """
-Copyright (c) 2020-2023, Mickaël 'Tiger-222' Schoentgen
+Copyright (c) 2020-2024, Mickaël 'Tiger-222' Schoentgen
 
 Permission to use, copy, modify, and distribute this software and its
 documentation for any purpose and without fee or royalty is hereby
