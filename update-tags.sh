5ce5289759ef1f9afe8de7b1690452e915ad8c55
Mickaël Schoentgen <contact@tiger-222.fr>
2024-07-30 16:22:08 +0200

    chore: smarter tag updater script

--- update-tags.sh
+++ update-tags.sh
@@ -3,9 +3,9 @@
 # Update associated release commit.
 #
 
-# XXX_LOCALES
-for tag in ca da de el en es fr it no pt ro ru sv; do
-    git tag -f -a "${tag}" -m "${tag}"
+for folder in $(find wikidict/lang/* -maxdepth 1 -type d); do
+    locale="${folder##*/}"
+    [ "${locale}" != "__pycache__" ] && git tag -f -a "${locale}" -m "${locale}"
 done
 
 git push -f --tags
