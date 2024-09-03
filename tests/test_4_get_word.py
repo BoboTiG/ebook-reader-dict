fd68fe8b8bbd4918a8a0daf55a98f05df67f049c
Mickaël Schoentgen <contact@tiger-222.fr>
2024-09-03 17:26:36 +0200

    tests: prevent regressions

--- tests/test_4_get_word.py
+++ tests/test_4_get_word.py
@@ -1,3 +1,5 @@
+import pytest
+
 from wikidict import get_word
 
 
@@ -24,3 +26,26 @@ def test_word_with_variants() -> None:
 
 def test_word_not_found() -> None:
     assert get_word.main("fr", "mutinerssssssss") == 0
+
+
+@pytest.mark.parametrize(
+    ("locale", "word", "pronunciations"),
+    [
+        ("sv", "en", ["/en/", "/eːn/", "/ɛn/"]),
+    ],
+)
+def test_locale_pronunciations(locale: str, word: str, pronunciations: list[str]) -> None:
+    details = get_word.get_word(word, locale)
+    assert details.pronunciations == pronunciations
+
+
+@pytest.mark.parametrize(
+    ("locale", "word", "etymology_len"),
+    [
+        ("es", "hocico", 1),
+        ("pt", "alguém", 1),
+    ],
+)
+def test_locale_etymologies(locale: str, word: str, etymology_len: int) -> None:
+    details = get_word.get_word(word, locale)
+    assert len(details.etymology) == etymology_len
