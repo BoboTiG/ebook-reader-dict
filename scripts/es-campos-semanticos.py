6d87120e8e07e1a791204ece4d0ca26455f935e8
Mickaël Schoentgen <contact@tiger-222.fr>
2024-09-19 10:17:52 +0200

    [ES] "es-campos-semanticos" script error

--- scripts/es-campos-semanticos.py
+++ scripts/es-campos-semanticos.py
@@ -1,58 +1,13 @@
-from scripts_utils import get_soup
-
-START_URL = "https://es.wiktionary.org/wiki/Categor%C3%ADa:Plantillas_de_campo_sem%C3%A1ntico"
-ROOT_URL = "https://es.wiktionary.org/"
-ALIAS_URL = "https://es.wiktionary.org/w/index.php?title=Especial:LoQueEnlazaAqu%C3%AD/{}&hidetrans=1&hidelinks=1"
-NEXTPAGE_TEXT = "página siguiente"
-
-
-def process_alias_page(model: str, template_text: str, results: dict[str, str]) -> None:
-    url = ALIAS_URL.format(model)
-    soup = get_soup(url)
-    ul = soup.find("ul", {"id": ["mw-whatlinkshere-list"]})
-    if not ul:
-        return
-    for alias in ul.find_all("a", {"class": ["mw-redirect"]}):
-        alias = alias.text.replace("Plantilla:", "")
-        if alias == "editar":
-            continue
-        results[alias] = template_text
-
-
-def process_cs_page(url: str, results: dict[str, str]) -> str:
-    soup = get_soup(url)
-
-    nextpage = ""
-    nextpage_div = soup.find(id="mw-pages")
-    last_link = nextpage_div.find_all("a")[-1]
-    if NEXTPAGE_TEXT == last_link.text:
-        nextpage = ROOT_URL + last_link.get("href")
-
-    divs_category = soup.find_all("div", {"class": "mw-category-group"})
-    for divs_category in divs_category:
-        lis = divs_category.find_all("li")
-        for li in lis:
-            template_link = li.find("a")
-            template_url = ROOT_URL + template_link.get("href")
-            template_name = template_link.text.split(":")[1]
-            template_soup = get_soup(template_url)
-            template_text_div = template_soup.find("div", {"class": "mw-parser-output"})
-            template_text = template_text_div.find("p").text.strip()
-            if template_text[-1] == ".":
-                template_text = template_text[:-1]
-            results[template_name] = template_text
-            process_alias_page(template_link.text, template_text, results)
-
-    return nextpage
-
+from scripts_utils import get_content
 
 results: dict[str, str] = {}
-next_page_url = START_URL
-while next_page_url:
-    next_page_url = process_cs_page(next_page_url, results)
+text = get_content("https://es.wiktionary.org/wiki/M%C3%B3dulo:contexto/csem?action=raw")
+code = text.splitlines()[1:-1]  # Skip "local m = {}" and "return m"
 
+script = "m = {}\n" + "\n".join(code)
+exec(script)
 
 print("campos_semanticos = {")
-for t, r in sorted(results.items()):
-    print(f'    "{t}": "{r}",')
-print(f"}}  # {len(results):,}")
+for t, r in sorted(m.items()):  # type: ignore[name-defined] # noqa: F821
+    print(f'    "{t}": "{r.pop()}",')
+print(f"}}  # {len(m):,}")  # type: ignore[name-defined] # noqa: F821
