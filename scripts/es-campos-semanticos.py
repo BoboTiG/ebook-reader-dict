5cbedbbd10cb10ec47ad6fa9b114763e8016cfef
Mickaël Schoentgen <contact@tiger-222.fr>
2024-10-05 20:45:43 +0200

    [ES] Improve es-campos-semanticos.py

--- scripts/es-campos-semanticos.py
+++ scripts/es-campos-semanticos.py
@@ -1,13 +1,16 @@
+import re
+
 from scripts_utils import get_content
 
 results: dict[str, str] = {}
 text = get_content("https://es.wiktionary.org/wiki/M%C3%B3dulo:contexto/csem?action=raw")
+text = re.sub(r'{("[^"]+"), "[^"]+"}', r"\1", text)
 code = text.splitlines()[1:-1]  # Skip "local m = {}" and "return m"
 
 script = "m = {}\n" + "\n".join(code)
 exec(script)
 
 print("campos_semanticos = {")
-for t, r in sorted(m.items()):  # type: ignore[name-defined] # noqa: F821
-    print(f'    "{t}": "{r.pop()}",')
+for key, value in sorted(m.items()):  # type: ignore[name-defined] # noqa: F821
+    print(f'    "{key}": "{value}",')
 print(f"}}  # {len(m):,}")  # type: ignore[name-defined] # noqa: F821
