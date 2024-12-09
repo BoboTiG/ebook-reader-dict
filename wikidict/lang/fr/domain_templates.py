ce503f918eee51156d759dbc2fc280ef34d6ac60
BoboTiG <BoboTiG@users.noreply.github.com>
2024-12-09 00:30:30 +0000

    Update locale-specific data

--- wikidict/lang/fr/domain_templates.py
+++ wikidict/lang/fr/domain_templates.py
@@ -212,6 +212,7 @@ domain_templates = {
     "métaplasmes": "Linguistique",
     "métaux": "Métallurgie",
     "métiers": "Métier",
+    "nerfs": "Anatomie",
     "nuages": "Météorologie",
     "nymphes": "Mythologie",
     "oblasts": "Géographie",
@@ -342,5 +343,5 @@ domain_templates = {
     "états": "État",
     "étoiles": "Astronomie",
     "îles": "Géographie",
-}  # 337
+}  # 338
 # END
