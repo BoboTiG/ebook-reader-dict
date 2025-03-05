0fdb7ef9d0a7ec12f8f2b0cac60d670a7630a8b3
Mickaël Schoentgen <contact@tiger-222.fr>
2025-03-05 21:26:14 +0100

    fix: update-tags.sh

--- update-tags.sh
+++ update-tags.sh
@@ -3,7 +3,7 @@
 # Update associated release commit.
 #
 
-for folder in $(find wikidict/lang/* -maxdepth 1 -type d); do
+for folder in $(find wikidict/lang/* -maxdepth 0 -type d); do
     locale="${folder##*/}"
     [ "${locale}" != "__pycache__" ] && git tag -f -a "${locale}" -m "${locale}"
 done
