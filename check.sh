9debfbe6ce5590d7db891ed0db63bd83f749901f
Mickaël Schoentgen <contact@tiger-222.fr>
2025-06-23 21:48:53 +0200

    fix: check.py for the CI

--- check.sh
+++ check.sh
@@ -2,6 +2,7 @@
 #
 # Small script to ensure quality checks pass before submitting a commit/PR.
 #
-./venv/bin/python -m ruff format wikidict tests scripts
-./venv/bin/python -m ruff check --fix --unsafe-fixes wikidict tests scripts
-./venv/bin/python -m mypy wikidict scripts tests
+[ -f ./venv/bin/python ] && python_exec='./venv/bin/python' || python_exec='python'
+$python_exec -m ruff format wikidict tests scripts
+$python_exec -m ruff check --fix --unsafe-fixes wikidict tests scripts
+$python_exec -m mypy wikidict scripts tests
