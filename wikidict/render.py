BoboTiG/ebook-reader-dict

commit 9df3937c58690f67940d85e281bafd38d17d3b96

fix: properly clean-up multiline `<ref>...</ref>`
Example: https://en.wiktionary.org/wiki/Augean

Related to #2471.

wikidict/render.py
@@ -460,6 +460,31 @@ def adjust_wikicode(code: str, locale: str) -> str:
     ''
     >>> adjust_wikicode("<!--\nsco\n-->", "it")
     ''
+
+    >>> adjust_wikicode("<ref name=oed/>Modelled<ref>Gerhard</ref> English<ref name=oed>Press.</ref>", "en")
+    'Modelled English'
+    >>> adjust_wikicode('From {{uder|en|la|Augeas}} {{suffix|en||an}}. {{w|Augeas}} is a figure in Greek mythology whose stables were never cleaned until {{w|Hercules}} was given the task of cleaning them.<ref name="AT">\n''Ariadne’s Thread: A Guide to International Tales Found in Classical Literature'' by William F. Hansen (2002; [http://www.cornellpress.cornell.edu/cup_detail.taf?ti_id=3674 Cornell University Press]; {{ISBN|9780801475726}}, 9780801436703), [http://books.google.co.uk/books?id=ezDlXl7gP9oC&pg=PA160&dq=%22Augean+stables%22&ei=ZAtOSoPJIY6-yQTn9ezvAg page 160]<br>  ''Herakles Cleans the Augean Stables''<br>  One of the best-known stories attached to Herakles tells how in one day he removed the dung from King Augeias’s cattle yard, which had not been cleaned in years.</ref>', "en")
+    'From {{uder|en|la|Augeas}} {{suffix|en||an}}. {{w|Augeas}} is a figure in Greek mythology whose stables were never cleaned until {{w|Hercules}} was given the task of cleaning them.'
+    >>> adjust_wikicode("<ref>{{Import:CFC}}</ref>", "en")
+    ''
+    >>> adjust_wikicode("<ref>{{Import:CFC}}</ref>bla bla bla <ref>{{Import:CFC}}</ref>", "en")
+    'bla bla bla '
+    >>> adjust_wikicode("<ref>{{Lit-Pfeifer: Etymologisches Wörterbuch|A=8}}, Seite 1551, Eintrag „Wein“<br />siehe auch: {{Literatur | Online=zitiert nach {{GBS|uEQtBgAAQBAJ|PA76|Hervorhebung=Wein}} | Autor=Corinna Leschber| Titel=„Wein“ und „Öl“ in ihren mediterranen Bezügen, Etymologie und Wortgeschichte | Verlag=Frank & Timme GmbH | Ort= | Jahr=2015 | Seiten=75–81 | Band=Band 24 von Forum: Rumänien, Culinaria balcanica, herausgegeben von Thede Kahl, Peter Mario Kreuter, Christina Vogel | ISBN=9783732901388}}.", "en")
+    ''
+    >>> adjust_wikicode('<ref name="CFC" />', "en")
+    ''
+    >>> adjust_wikicode('<ref name="CFC">{{Import:CFC}}</ref>', "en")
+    ''
+    >>> adjust_wikicode('<ref name="CFC">{{CFC\\n|foo}}</ref>', "en")
+    ''
+    >>> adjust_wikicode("<ref>D'après ''Dictionnaire du tapissier : critique et historique de l’ameublement français, depuis les temps anciens jusqu’à nos jours'', par J. Deville, page 32 ({{Gallica|http://gallica.bnf.fr/ark:/12148/bpt6k55042642/f71.image}})</ref>", "en")
+    ''
+    >>> adjust_wikicode("<ref>", "en")
+    ''
+    >>> adjust_wikicode("</ref>", "en")
+    ''
+    >>> adjust_wikicode('<ref name="Marshall 2001"><sup>he</sup></ref>', "en")
+    ''
     """
 
     # Namespaces (moved from `utils.clean()` to be able to filter on multiple lines)
@@ -501,6 +526,17 @@ def adjust_wikicode(code: str, locale: str) -> str:
 
     # {{!}} → "|"
     # code = code.replace("{{!}}", "|")
+
+    # <ref name="CFC"/> → ''
+    code = re.sub(r"<ref[^>]*/>", "", code)
+    # <ref>foo → ''
+    # <ref>foo</ref> → ''
+    # <ref name="CFC">{{Import:CFC}}</ref> → ''
+    # <ref name="CFC"><tag>...</tag></ref> → ''
+    code = re.sub(r"<ref[^>]*/?>[\s\S]*?(?:</ref>|$)", "", code)
+    # <ref> → ''
+    # </ref> → ''
+    code = code.replace("<ref>", "").replace("</ref>", "")
 
     func: Callable[[str, str], str] = lang.adjust_wikicode[locale]
     return func(code, locale)

wikidict/utils.py
@@ -570,31 +570,11 @@ def clean(text: str) -> str:
         '{{Lien web|url=http://stella.atilf.fr/few/|titre=Französisches Etymologisches Wörterbuch}}'
         >>> clean("d'<nowiki/>''Arvernus'', surnom ethnique, ou composé d'''are''")
         "d'<i>Arvernus</i>, surnom ethnique, ou composé d'<i>are</i>"
-        >>> clean("<ref name=oed/>Modelled<ref>Gerhard</ref> English<ref name=oed>Press.</ref>")
-        'Modelled English'
 
         >>> clean("")
         ''
         >>> clean("<span style='color:black'>[[♣]]</span>")
         "<span style='color:black'>♣</span>"
-        >>> clean("<ref>{{Import:CFC}}</ref>")
-        ''
-        >>> clean("<ref>{{Import:CFC}}</ref>bla bla bla <ref>{{Import:CFC}}</ref>")
-        'bla bla bla'
-        >>> clean("<ref>{{Lit-Pfeifer: Etymologisches Wörterbuch|A=8}}, Seite 1551, Eintrag „Wein“<br />siehe auch: {{Literatur | Online=zitiert nach {{GBS|uEQtBgAAQBAJ|PA76|Hervorhebung=Wein}} | Autor=Corinna Leschber| Titel=„Wein“ und „Öl“ in ihren mediterranen Bezügen, Etymologie und Wortgeschichte | Verlag=Frank & Timme GmbH | Ort= | Jahr=2015 | Seiten=75–81 | Band=Band 24 von Forum: Rumänien, Culinaria balcanica, herausgegeben von Thede Kahl, Peter Mario Kreuter, Christina Vogel | ISBN=9783732901388}}.")
-        ''
-        >>> clean('<ref name="CFC" />')
-        ''
-        >>> clean('<ref name="CFC">{{Import:CFC}}</ref>')
-        ''
-        >>> clean('<ref name="CFC">{{CFC\\n|foo}}</ref>')
-        ''
-        >>> clean("<ref>D'après ''Dictionnaire du tapissier : critique et historique de l’ameublement français, depuis les temps anciens jusqu’à nos jours'', par J. Deville, page 32 ({{Gallica|http://gallica.bnf.fr/ark:/12148/bpt6k55042642/f71.image}})</ref>")
-        ''
-        >>> clean("<ref>")
-        ''
-        >>> clean("</ref>")
-        ''
         >>> clean("''italic''")
         '<i>italic</i>'
         >>> clean("'''strong'''")
@@ -632,8 +612,6 @@ def clean(text: str) -> str:
         >>> clean("[[http://www.tv5monde.com/cms/chaine-francophone/lf/Merci-Professeur/p-17081-Une-peur-bleue.htm?episode=10 Voir aussi l’explication de Bernard Cerquiglini en images]]")
         ''
 
-        >>> clean('<ref name="Marshall 2001"><sup>he</sup></ref>')
-        ''
         >>> clean("<nowiki/>")
         ''
         >>> clean("<nowiki>«</nowiki>")
@@ -690,18 +668,6 @@ def clean(text: str) -> str:
 
     # Remove line breaks
     text = text.replace("\n", "")
-
-    # Parser hooks
-    # <ref name="CFC"/> → ''
-    text = sub(r"<ref[^>]*/>", "", text)
-    # <ref>foo → ''
-    # <ref>foo</ref> → ''
-    # <ref name="CFC">{{Import:CFC}}</ref> → ''
-    # <ref name="CFC"><tag>...</tag></ref> → ''
-    text = sub(r"<ref[^>]*/?>[\s\S]*?(?:</ref>|$)", "", text)
-    # <ref> → ''
-    # </ref> → ''
-    text = text.replace("<ref>", "").replace("</ref>", "")
 
     # HTML
     # Source: https://github.com/5j9/wikitextparser/blob/b24033b/wikitextparser/_wikitext.py#L83

commit e37abd2d4a862ecc3618d0b14199fe2ffec84c67

fix: regression introduced in 9df3937c58690f67940d85e281bafd38d17d3b96
Example: https://fr.wiktionary.org/wiki/a

Related to #2482.

wikidict/render.py
@@ -487,6 +487,10 @@ def adjust_wikicode(code: str, locale: str) -> str:
     ''
     >>> adjust_wikicode('<ref name="Marshall 2001"><sup>he</sup></ref>', "en")
     ''
+    >>> adjust_wikicode('a<references></references>b', "fr")
+    'ab'
+    >>> adjust_wikicode('a<references>xcv</references>b', "fr")
+    'ab'
     """
 
     # Namespaces (moved from `utils.clean()` to be able to filter on multiple lines)
@@ -535,7 +539,7 @@ def adjust_wikicode(code: str, locale: str) -> str:
     # <ref>foo</ref> → ''
     # <ref name="CFC">{{Import:CFC}}</ref> → ''
     # <ref name="CFC"><tag>...</tag></ref> → ''
-    code = re.sub(r"<ref[^>]*/?>[\s\S]*?(?:</ref>|$)", "", code)
+    code = re.sub(r"<ref[^>]*/?>[\s\S]*?(?:</\s*ref[^>]*>|$)", "", code)
     # <ref> → ''
     # </ref> → ''
     code = code.replace("<ref>", "").replace("</ref>", "")