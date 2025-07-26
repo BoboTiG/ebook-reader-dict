BoboTiG/ebook-reader-dict

commit 792abfd5207fc019129374d748c8efaaa180ce9f

feat: part of speech (POS) separation (#2464)
* feat: part of speech (POS) separation

* fix: clean-up

* feat: continuing ...

* feat: primary console markup for --get-word

* feat: continuing ...

* feat: continuing ...

* fix: adjustments

wikidict/convert.py
@@ -36,27 +36,30 @@ if TYPE_CHECKING:
 # Kobo-related dictionaries
 WORD_TPL_KOBO = Template(
     """\
-<w><p><a name="{{ word }}"/><b>{{ current_word }}</b>{{ pronunciation }}{{ gender }}<br/><br/><ol>
-{%- for definition in definitions -%}
-    {%- if definition is string -%}
-        <li>{{ definition }}</li>
-    {%- else -%}
-        <ol style="list-style-type:lower-alpha">
-        {%- for sub_def in definition -%}
-            {%- if sub_def is string -%}
-                <li>{{ sub_def }}</li>
-            {%- else -%}
-                <ol style="list-style-type:lower-roman">
-                    {%- for sub_sub_def in sub_def -%}
-                        <li>{{ sub_sub_def }}</li>
-                    {%- endfor -%}
-                </ol>
-            {%- endif -%}
-        {%- endfor -%}
-        </ol>
-    {%- endif -%}
+<w><p><a name="{{ word }}"/><b>{{ current_word }}</b>{{ pronunciation }}{{ gender }}<br/><br/>
+{%- for pos, pos_definitions in definitions -%}
+    <b>{{ pos }}</b><ol>
+    {%- for definition in pos_definitions -%}
+        {%- if definition is string -%}
+            <li>{{ definition }}</li>
+        {%- else -%}
+            <ol style="list-style-type:lower-alpha">
+            {%- for sub_def in definition -%}
+                {%- if sub_def is string -%}
+                    <li>{{ sub_def }}</li>
+                {%- else -%}
+                    <ol style="list-style-type:lower-roman">
+                        {%- for sub_sub_def in sub_def -%}
+                            <li>{{ sub_sub_def }}</li>
+                        {%- endfor -%}
+                    </ol>
+                {%- endif -%}
+            {%- endfor -%}
+            </ol>
+        {%- endif -%}
+    {%- endfor -%}
+    </ol>
 {%- endfor -%}
-</ol>
 {%- if etymologies -%}
     {%- for etymology in etymologies -%}
         {%- if etymology is string -%}
@@ -98,8 +101,10 @@ WORD_TPL_DICTFILE = Template(
 {%- for variant in variants %}
 & {{ variant }}
 {%- endfor %}
-<html><ol>
-    {%- for definition in definitions -%}
+<html>
+{%- for pos, pos_definitions in definitions -%}
+    <b>{{ pos }}</b><ol>
+    {%- for definition in pos_definitions -%}
         {%- if definition is string -%}
             <li>{{ definition }}</li>
         {%- else -%}
@@ -118,7 +123,8 @@ WORD_TPL_DICTFILE = Template(
             </ol>
         {%- endif -%}
     {%- endfor -%}
-</ol>
+    </ol>
+{%- endfor -%}
 {%- if etymologies -%}
     {%- for etymology in etymologies -%}
         {%- if etymology is string -%}
@@ -248,7 +254,7 @@ class BaseFormat:
                 self.template,
                 word=word,
                 current_word=(current_word if isinstance(self, KoboFormat) or current_word != word else ""),
-                definitions=current_details.definitions,
+                definitions=current_details.definitions.items(),
                 pronunciation=utils.convert_pronunciation(current_details.pronunciations)
                 if current_details.pronunciations
                 else "",

tests/test_3_convert.py
@@ -35,38 +35,40 @@ Version sans étymologies :
 Mis à jour le"""
 
 WORDS = {
-    "empty": Word([], [], [], [], []),
-    "foo": Word(["pron"], ["gender"], ["etyl"], ["def 1", ("sdef 1",)], []),
-    "foos": Word(["pron"], ["gender"], ["etyl"], ["def 1", ("sdef 1", ("ssdef 1",))], ["baz"]),
-    "baz": Word(["pron"], ["gender"], ["etyl"], ["def 1", ("sdef 1",)], ["foobar"]),
-    "empty1": Word([], [], [], [], ["foo"]),
-    "empty2": Word([], [], [], [], ["empty1"]),
+    "empty": Word([], [], [], {}, []),
+    "foo": Word(["pron"], ["gender"], ["etyl"], {"Noun": ["def 1", ("sdef 1",)]}, []),
+    "foos": Word(["pron"], ["gender"], ["etyl"], {"Noun": ["def 1", ("sdef 1", ("ssdef 1",))]}, ["baz"]),
+    "baz": Word(["pron"], ["gender"], ["etyl"], {"Noun": ["def 1", ("sdef 1",)]}, ["foobar"]),
+    "empty1": Word([], [], [], {}, ["foo"]),
+    "empty2": Word([], [], [], {}, ["empty1"]),
     "Multiple Etymologies": Word(
         ["pron"],
         ["gender"],
         ["etyl 1", ("setyl 1",)],
-        ["def 1", ("sdef 1",)],
+        {"Noun": ["def 1", ("sdef 1",)]},
         [],
     ),
     "Multiple Etymology": Word(
         ["pron0"],
         ["gender0"],
         ["etyl0"],
-        ["def 0"],
+        {"Noun": ["def 0"]},
         ["Multiple Etymologies"],
     ),
     "GIF": Word(
         ["pron"],
         ["gender"],
         ["etyl"],
-        [
-            '<img style="height:100%;max-height:0.8em;width:auto;vertical-align:bottom"'
-            ' src="data:image/gif;base64,R0lGODdhNwAZAIEAAAAAAP///wAAAAAAACwAAAAANwAZAE'
-            "AIwwADCAwAAMDAgwgTKlzIUKDBgwUZFnw4cGLDihEvOjSYseFEigQtLhSpsaNGiSdTQgS5kiVG"
-            "lwhJeuRoMuHHkDBH1pT4cKdKmSpjUjT50efGnEWTsuxo9KbQnC1TFp051KhNpUid8tR6EijPkC"
-            "V3en2J9erLoBjRXl1qVS1amTWn6oSK1WfGpnjDQo1q1Wvbs125PgX5l6zctW1JFgas96/FxYwv"
-            'RnQsODHkyXuPDt5aVihYt5pBr9woGrJktmpNfxUYEAA7"/>'
-        ],
+        {
+            "Noun": [
+                '<img style="height:100%;max-height:0.8em;width:auto;vertical-align:bottom"'
+                ' src="data:image/gif;base64,R0lGODdhNwAZAIEAAAAAAP///wAAAAAAACwAAAAANwAZAE'
+                "AIwwADCAwAAMDAgwgTKlzIUKDBgwUZFnw4cGLDihEvOjSYseFEigQtLhSpsaNGiSdTQgS5kiVG"
+                "lwhJeuRoMuHHkDBH1pT4cKdKmSpjUjT50efGnEWTsuxo9KbQnC1TFp051KhNpUid8tR6EijPkC"
+                "V3en2J9erLoBjRXl1qVS1amTWn6oSK1WfGpnjDQo1q1Wvbs125PgX5l6zctW1JFgas96/FxYwv"
+                'RnQsODHkyXuPDt5aVihYt5pBr9woGrJktmpNfxUYEAA7"/>'
+            ]
+        },
         ["gif"],
     ),
 }
@@ -303,16 +305,16 @@ def test_generate_secondary_dict(formatter: type[convert.BaseFormat], filename: 
 
 
 FORMATTED_WORD_KOBO = """\
-<w><p><a name="Multiple Etymologies"/><b>Multiple Etymologies</b> pron <i>gender</i>.<br/><br/><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></p><var><variant name="multiple etymology"/></var></w>
+<w><p><a name="Multiple Etymologies"/><b>Multiple Etymologies</b> pron <i>gender</i>.<br/><br/><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></p><var><variant name="multiple etymology"/></var></w>
 """
 FORMATTED_WORD_KOBO_NO_ETYMOLOGY = """\
-<w><p><a name="Multiple Etymologies"/><b>Multiple Etymologies</b> pron <i>gender</i>.<br/><br/><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></p><var><variant name="multiple etymology"/></var></w>
+<w><p><a name="Multiple Etymologies"/><b>Multiple Etymologies</b> pron <i>gender</i>.<br/><br/><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></p><var><variant name="multiple etymology"/></var></w>
 """
 FORMATTED_WORD_DICTFILE = """\
 @ Multiple Etymologies
 : pron <i>gender</i>.
 & Multiple Etymology
-<html><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></html>\
+<html><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></html>\
 
 
 """
@@ -320,7 +322,7 @@ FORMATTED_WORD_DICTFILE_NO_ETYMOLOGY = """\
 @ Multiple Etymologies
 : pron <i>gender</i>.
 & Multiple Etymology
-<html><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></html>\
+<html><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></html>\
 
 
 """
@@ -359,42 +361,44 @@ WORDS_VARIANTS_FR = words = {
         pronunciations=["\\ɛtʁ\\"],
         genders=[],
         etymology=[],
-        definitions=["Définition de 'estre'."],
+        definitions={"Verbe": ["Définition de 'estre'."]},
         variants=[],
     ),
     "être": Word(
         pronunciations=["\\ɛtʁ\\"],
         genders=["m"],
         etymology=[],
-        definitions=[
-            "Définition de 'être'.",
-        ],
+        definitions={
+            "Verbe": [
+                "Définition de 'être'.",
+            ]
+        },
         variants=[],
     ),
     "suis": Word(
         pronunciations=["\\sɥi\\"],
         genders=[],
         etymology=[],
-        definitions=[],
+        definitions={},
         variants=["suivre", "être", "estre"],
     ),
     "suivre": Word(
         pronunciations=["\\sɥivʁ\\"],
         genders=[],
         etymology=[],
-        definitions=["Définition de 'suivre'."],
+        definitions={"Verbe": ["Définition de 'suivre'."]},
         variants=[],
     ),
 }
 WORDS_VARIANTS_ES = {
-    "gastadan": Word(pronunciations=[], genders=[], etymology=[], definitions=[], variants=["gastada"]),
-    "gastada": Word(pronunciations=[], genders=[], etymology=[], definitions=[], variants=["gastado"]),
-    "gastado": Word(pronunciations=[], genders=[], etymology=[], definitions=[], variants=["gastar"]),
+    "gastadan": Word(pronunciations=[], genders=[], etymology=[], definitions={}, variants=["gastada"]),
+    "gastada": Word(pronunciations=[], genders=[], etymology=[], definitions={}, variants=["gastado"]),
+    "gastado": Word(pronunciations=[], genders=[], etymology=[], definitions={}, variants=["gastar"]),
     "gastar": Word(
         pronunciations=[],
         genders=[],
         etymology=[],
-        definitions=["Definition of 'gastar'."],
+        definitions={"Verb": ["Definition of 'gastar'."]},
         variants=[],
     ),
 }
@@ -469,28 +473,28 @@ def test_df_format(tmp_path: Path) -> None:
         output.read_text(encoding="utf-8")
         == r"""@ estre
 : \ɛtʁ\
-<html><ol><li>Définition de 'estre'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'estre'.</li></ol></html>
 
 @ être
 : \ɛtʁ\ <i>m</i>.
-<html><ol><li>Définition de 'être'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'être'.</li></ol></html>
 
 @ suis
 : <b>estre</b> \ɛtʁ\
-<html><ol><li>Définition de 'estre'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'estre'.</li></ol></html>
 
 @ suis
 : <b>suivre</b> \sɥivʁ\
-<html><ol><li>Définition de 'suivre'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'suivre'.</li></ol></html>
 
 @ suis
 : <b>être</b> \ɛtʁ\ <i>m</i>.
-<html><ol><li>Définition de 'être'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'être'.</li></ol></html>
 
 @ suivre
 : \sɥivʁ\
 & suis
-<html><ol><li>Définition de 'suivre'.</li></ol></html>
+<html><b>Verbe</b><ol><li>Définition de 'suivre'.</li></ol></html>
 
 """
     )

tests/test_2_render.py
@@ -1,7 +1,7 @@
 import logging
 from collections.abc import Callable
 from pathlib import Path
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 from wikitextparser import Section
@@ -42,19 +42,12 @@ def test_render_word_with_empty_subdefinition(page: Callable[[str, str], str]) -
     assert details
 
     defs = details.definitions
-    assert len(defs) == 2
-    assert isinstance(defs[0], str)
-    assert isinstance(defs[1], tuple)
-
-    subdefs = defs[1]
-    assert len(subdefs) == 2
-    assert isinstance(subdefs[0], str)
-    assert isinstance(subdefs[1], tuple)
-
-    subsubdefs = subdefs[1]
-    assert len(subsubdefs) == 1
-    assert isinstance(subsubdefs[0], str)
-    assert subsubdefs[0]
+    assert defs == {
+        "Nom": [
+            "<i>(Botanique)</i> Espèce de mauves, grandes plantes laineuses aux feuilles entières ou à 3 lobes et à bordure dentée, et aux fleurs assez grandes de couleur blanc rosé, avec les anthères des étamines rougeâtres.",
+            ("Sub sub list with empty definition", ("ok",)),
+        ],
+    }
 
 
 def test_find_section_definitions_and_es_replace_defs_list_with_numbered_lists() -> None:
@@ -135,28 +128,34 @@ def test_missing_templates(keep_unfinished: bool, workers: int, caplog: pytest.L
             pronunciations=[],
             genders=[],
             etymology=[],
-            definitions=[
-                "Première lettre et première voyelle de l’alphabet latin (minuscule). {{unknown-1}}.",
-                "Chiffre hexadécimal dix (minuscule) {{unknown-2}}.",
-                "{{unknown-3}}",
-            ],
+            definitions={
+                "Lettre": [
+                    "Première lettre et première voyelle de l’alphabet latin (minuscule). {{unknown-1}}.",
+                    "Chiffre hexadécimal dix (minuscule) {{unknown-2}}.",
+                    "{{unknown-3}}",
+                ]
+            },
             variants=[],
         ),
         "b": Word(
             pronunciations=[],
             genders=[],
             etymology=[],
-            definitions=["Deuxième lettre et première consonne de l’alphabet latin (minuscule). {{unknown-1}}."],
+            definitions={
+                "Lettre": ["Deuxième lettre et première consonne de l’alphabet latin (minuscule). {{unknown-1}}."]
+            },
             variants=[],
         ),
         "c": Word(
             pronunciations=[],
             genders=[],
             etymology=[],
-            definitions=[
-                "Troisième lettre et deuxième consonne de l’alphabet latin (minuscule). {{unknown-1}}.",
-                "{{unknown-3}}",
-            ],
+            definitions={
+                "Lettre": [
+                    "Troisième lettre et deuxième consonne de l’alphabet latin (minuscule). {{unknown-1}}.",
+                    "{{unknown-3}}",
+                ]
+            },
             variants=[],
         ),
     }
@@ -189,6 +188,7 @@ def test_sublang(locale: str, lang_src: str, lang_dst: str, tmp_path: Path) -> N
             patch.object(render, "load") as mocked_l,
             patch.object(render, "render") as mocked_r,
             patch.object(render, "save") as mocked_s,
+            patch.object(render, "show_pos", MagicMock()),
         ):
             mocked_gljf.return_value = pages
             mocked_l.return_value = words

commit 2d6ca3d787f58110f305e35a70f3c8f5b9b5a7b6

fix:(dictfile, and co.): add back a line space between pronunciations/genders and the POS
Regression introduced in 792abfd5207fc019129374d748c8efaaa180ce9f where
the start of definitions was a list tag (`<li>`) which is seen as a "block display"
while the new code was using a strong tag (`<b>`) which is seens as "inline display".

Fixing by using a paragraph (`<p>`) since it is also the standard/recommended way.

I also tried to use a line break (`<br/>`) before the `<b>`, but the output was not satisfying:
the first line break between the pronunciations/genders and the POS was too small.

Fixes #2477.

wikidict/convert.py
@@ -105,7 +105,7 @@ WORD_TPL_DICTFILE = Template(
 {%- endfor %}
 <html>
 {%- for pos, pos_definitions in definitions -%}
-    <b>{{ pos }}</b><ol>
+    <p><b>{{ pos }}</b></p><ol>
     {%- for definition in pos_definitions -%}
         {%- if definition is string -%}
             <li>{{ definition }}</li>

tests/test_3_convert.py
@@ -312,7 +312,7 @@ FORMATTED_WORD_DICTFILE = """\
 @ Multiple Etymologies
 : pron <i>gender</i>.
 & Multiple Etymology
-<html><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></html>\
+<html><p><b>Noun</b></p><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol><p>etyl 1</p><ol><li>setyl 1</li></ol><br/></html>\
 
 
 """
@@ -320,7 +320,7 @@ FORMATTED_WORD_DICTFILE_NO_ETYMOLOGY = """\
 @ Multiple Etymologies
 : pron <i>gender</i>.
 & Multiple Etymology
-<html><b>Noun</b><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></html>\
+<html><p><b>Noun</b></p><ol><li>def 1</li><ol style="list-style-type:lower-alpha"><li>sdef 1</li></ol></ol></html>\
 
 
 """
@@ -503,28 +503,28 @@ def test_df_format(tmp_path: Path) -> None:
         output.read_text(encoding="utf-8")
         == r"""@ estre
 : \ɛtʁ\
-<html><b>Verbe</b><ol><li>Définition de 'estre'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'estre'.</li></ol></html>
 
 @ être
 : \ɛtʁ\ <i>m</i>.
-<html><b>Verbe</b><ol><li>Définition de 'être'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'être'.</li></ol></html>
 
 @ suis
 : <b>estre</b> \ɛtʁ\
-<html><b>Verbe</b><ol><li>Définition de 'estre'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'estre'.</li></ol></html>
 
 @ suis
 : <b>suivre</b> \sɥivʁ\
-<html><b>Verbe</b><ol><li>Définition de 'suivre'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'suivre'.</li></ol></html>
 
 @ suis
 : <b>être</b> \ɛtʁ\ <i>m</i>.
-<html><b>Verbe</b><ol><li>Définition de 'être'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'être'.</li></ol></html>
 
 @ suivre
 : \sɥivʁ\
 & suis
-<html><b>Verbe</b><ol><li>Définition de 'suivre'.</li></ol></html>
+<html><p><b>Verbe</b></p><ol><li>Définition de 'suivre'.</li></ol></html>
 
 """
     )